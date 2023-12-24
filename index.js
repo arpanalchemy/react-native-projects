1a45174c28ca43310cb0b48682df4d96037e1dce
Arpan Kayastha <141130320+ArpanGrip@users.noreply.github.com>
2023-12-24 22:56:59 +0530

    Project5: roll the dice

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import AppPro from './AppPro';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppPro);
+import {name as appName} from './app.json';
+import App from './src/App';
+
+AppRegistry.registerComponent(appName, () => App);
